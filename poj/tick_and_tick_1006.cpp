538b2019d726ad3bc04ae6773ee78b295936b5b5
fanxi.rd <fancy2110@gmail.com>
2019-01-08 18:18:23 +0800

    feat: 完成1006

---
+++ poj/tick_and_tick_1006.cpp
@@ -0,0 +1,36 @@
+/***
+ * 
+ * The three hands of the clock are rotating every second and meeting each other many times everyday.
+ * Finally, they get bored of this and each of them would like to stay away from the other two. 
+ * A hand is happy if it is at least D degrees from any of the rest. You are to calculate how much time in a day that all the hands are happy.
+ * 
+ * test case :
+ * 0
+ * 120
+ * 90
+ * -1
+ * 
+ * result:
+ * 100.000
+ * 0.000
+ * 6.251
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    int angle = 0 
+    do {
+        cin >> angle;
+        if (angle < 0) break;
+
+        int a,b,c = 0;
+         
+        
+
+    } while(true)
+    /* code */
+    return 0;
+}
