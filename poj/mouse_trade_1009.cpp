a69cfeaaa4154a33491ec7b4a95d9d1d7d66a5bf
fanxi.rd <fancy2110@gmail.com>
2019-03-28 11:17:43 +0800

    feat: 调整输出方式

--- poj/mouse_trade_1009.cpp
+++ poj/mouse_trade_1009.cpp
@@ -21,18 +21,21 @@ test case
 100 0
 300 0
 -1 -1
+
 */
 
 #include <iostream>
+#include <algorithm>
 #include <iomanip>
 #include <math.h>
+#include <stdio.h>
 #include <float.h>
 
 struct node
 {
-    int food;
-    int money;
-    float price;
+    double food;
+    double money;
+    double price;
 };
 
 struct node rooms[1001];
@@ -40,47 +43,30 @@ int items[1001];
 
 using namespace std;
 
-int compare_price(int left_index, int right_index)
+void show_items(int *ref_items, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        struct node &item = rooms[ref_items[i]];
+        cout << "sort: " << i << "; P(" << item.food << "," << item.money << "," << item.price << ")" << endl;
+    }
+
+    cout << endl;
+}
+
+bool compare_price(int left_index, int right_index)
 {
     struct node &left = rooms[left_index];
     struct node &right = rooms[right_index];
 
-    if (left.price < right.price)
-        return -1;
-    if (left.price == right.price)
-        return 0;
-    return 1;
-}
-
-void sort(int *items, int start, int end)
-{
-    if (start >= end)
-        return;
-
-    int mid = items[start];
-    int i = start;
-    int j = end;
-
-    while (i < j)
-    {
-        while (i < j && compare_price(items[j], mid) < 0)
-            j--;
-        items[i] = items[j];
-
-        while (i < j && compare_price(items[i], mid) >= 0)
-            i++;
-        items[j] = items[i];
-    }
-
-    items[i] = mid;
-    sort(items, start, i);
-    sort(items, i + 1, end);
+    return left.price > right.price;
 }
 
 int main(int argc, char const *argv[])
 {
     int n = 0;
-    int sum = 0;
+    double sum = 0.0;
+    cout.setf(ios::fixed);
     do
     {
         cin >> sum >> n;
@@ -89,7 +75,7 @@ int main(int argc, char const *argv[])
 
         // cout << "sum:" << sum << ", n:" << n << endl;
 
-        float total = 0.0f;
+        double total = 0.0f;
         int index = 0;
         int count = 0;
 
@@ -103,7 +89,7 @@ int main(int argc, char const *argv[])
             }
             else
             {
-                room.price = room.food * 1.0f / room.money;
+                room.price = room.food / room.money;
                 items[index] = index;
                 index++;
             }
@@ -111,11 +97,12 @@ int main(int argc, char const *argv[])
             count++;
         }
 
-        // cout << "sum:" << sum << ", total:" << total << endl;
-        sort(items, 0, index - 1);
+        // show_items(items, index);
+        sort(items, items + index, compare_price);
+        // show_items(items, index);
 
         int item_index = 0;
-        while (sum > 0 && index > 0 && index < item_index)
+        while (sum > 0 && index > 0 && index > item_index)
         {
             struct node &room = rooms[items[item_index++]];
             if (sum >= room.money)
@@ -132,8 +119,13 @@ int main(int argc, char const *argv[])
             // cout << "sum:" << sum << ", total:" << total << endl;
         }
 
-        cout << fixed << setprecision(3) << total << endl;
+        // cout << setprecision(3) << total << endl; 此输出不能满足需求，无法ac
+        printf("%.3lf\n", total);
     } while (true);
+
+    // double total = 12.3456789;
+    // cout << setprecision(3) << total << endl;
+    // printf("%.3lf\n", total);
     /* code */
     return 0;
 }
